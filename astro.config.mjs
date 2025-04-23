13b37cea143cd4edf37defa58e2b42a18fc7f6b9
Testin232 <97626264+Testin232@users.noreply.github.com>
2025-04-23 14:08:50 +0200

    Updated the footer and added support for Icons.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,17 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import cloudflare from "@astrojs/cloudflare";
+import astroIcon from "astro-icon"; // 👈 Add this line
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
-  integrations: [mdx(), sitemap()],
+  site: "https://t1grid.com",
+  integrations: [
+    mdx(),
+    sitemap(),
+    astroIcon(), // 👈 Add this line to the integrations array
+  ],
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
